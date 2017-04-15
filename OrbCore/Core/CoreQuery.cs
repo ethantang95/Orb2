9e82f2dbbb26d74a841770c7318ba08427f4fc5c
Ethan Tang <ethan.tang95@outlook.com>
2017-04-14 23:57:01 -0400

    Added Tests to the simple cache

--- OrbCore/Core/CoreQuery.cs
+++ OrbCore/Core/CoreQuery.cs
@@ -4,8 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace OrbCore.Core {
     public class CoreQuery : ICoreQuery {
+        public IChannel GetChannel(ulong channelId) {
+            throw new NotImplementedException();
+        }
+
+        public IGuild GetGuild(ulong guildId) {
+            throw new NotImplementedException();
+        }
+
+        public IMessage GetMessage(ulong messageId) {
+            throw new NotImplementedException();
+        }
+
+        public IRole GetRole(ulong roleId) {
+            throw new NotImplementedException();
+        }
+
+        public IUser GetUser(ulong userId) {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ OrbCoreTests/CacheTests/SimpleCacheTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using OrbCore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbCoreTests.CacheTests {
+    [TestFixture]
+    class SimpleCacheTests {
+        SimpleCache<int> _cache;
+        ulong _idCount;
+
+        [OneTimeSetUp]
+        public void SetUp() {
+            _cache = new SimpleCache<int>();
+            _idCount = 0;
+        }
+
+        [Test]
+        public void TestAdd() {
+            var id = GetAndUpdateId();
+            _cache.SetMember(id, 1);
+            Assert.AreEqual(1, _cache.GetMember(id).Value);
+        }
+
+        [Test]
+        public void TestUpdate() {
+            var id = GetAndUpdateId();
+            _cache.SetMember(id, 1);
+            _cache.SetMember(id, 2);
+            Assert.AreEqual(2, _cache.GetMember(id).Value);
+        }
+
+        [Test]
+        public void TestChangeIfExistWhenExists() {
+            var id = GetAndUpdateId();
+            _cache.SetMember(id, 1);
+            _cache.ChangeIfExists(id, 2);
+            Assert.AreEqual(2, _cache.GetMember(id).Value);
+        }
+
+        [Test]
+        public void TestChangeIfExistWhenNotExists() {
+            var id = GetAndUpdateId();
+            _cache.ChangeIfExists(id, 2);
+            Assert.IsFalse(_cache.GetMember(id).Present);
+        }
+
+        [Test]
+        public void TestGettingNonExistentMember() {
+            var id = GetAndUpdateId();
+            Assert.IsFalse(_cache.GetMember(id).Present);
+        }
+
+        [Test]
+        public void TestGetMemberOrCall() {
+            var id = GetAndUpdateId();
+            Assert.AreEqual(1, _cache.GetMemberOrCall(id, (s) => { return 1; }));
+            Assert.AreEqual(1, _cache.GetMember(id).Value);
+        }
+
+        [Test]
+        public void TestRemoveMember() {
+            var id = GetAndUpdateId();
+            _cache.SetMember(id, 1);
+            Assert.AreEqual(1, _cache.GetMember(id).Value);
+            _cache.RemoveMember(id);
+            Assert.IsFalse(_cache.GetMember(id).Present);
+        }
+
+        [Test]
+        public void TestHasMemberExists() {
+            var id = GetAndUpdateId();
+            _cache.SetMember(id, 1);
+            Assert.IsTrue(_cache.HasMember(id));
+        }
+
+        [Test]
+        public void TestHasMemberNotExists() {
+            var id = GetAndUpdateId();
+            Assert.IsFalse(_cache.HasMember(id));
+        }
+
+        private ulong GetAndUpdateId() {
+            _idCount++;
+            return _idCount;
+        }
+    }
+}
